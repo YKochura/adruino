5280562668c5d6f9fece22b316a5829130b3b3ea
YKochura <iuriy.kochura@gmail.com>
2019-12-06 10:59:01 +0200

    first commit

---
+++ soundSensor_LED/soundSensor_LED.ino
@@ -0,0 +1,24 @@
+boolean state;
+#define ledPin 13
+#define soundSensorPin 7
+
+void setup() {
+  pinMode(ledPin OUTPUT);
+  pinMode(soundSensorPin, INPUT);
+  state = false;
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  Serial.println(digitalRead(7));
+  if( digitalRead(7) == 1){
+    state =! state;
+    digitalWrite(13, state);
+    delay(100);
+  }
+
+  
+  
+
+}
